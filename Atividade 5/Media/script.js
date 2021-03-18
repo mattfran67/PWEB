a12083972b2674a1f9e6a47d2fb133e468e4bf15
Mateus Gonçalves Franco <39377546+mattfran67@users.noreply.github.com>
2021-03-17 21:49:11 -0300

    Update script.js

--- Atividade 5/Media/script.js
+++ Atividade 5/Media/script.js
@@ -3,8 +3,6 @@ const nota1 = Number(prompt("Digite a nota 1:"));
 const nota2 = Number(prompt("Digite a nota 2:"));
 const nota3 = Number(prompt("Digite a nota 3:"));
 
-console.log(nota1);
-
 if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)) {
   alert("Nota Inválida!");
 } else {
