8bf4794a14134a72869c90c413673b8698581587
Mateus Gonçalves Franco <39377546+mattfran67@users.noreply.github.com>
2021-03-24 22:02:41 -0300

    Add files via upload

---
+++ Atividade 6/script.js
@@ -0,0 +1,20 @@
+const player = Number(prompt('Escolha:\n1. Pedra\n2. Papel\n3. Tesoura'));
+
+const jogadas = ['Pedra', 'Papel', 'Tesoura']; 
+
+// jogada da maquina
+const maquina = Math.floor(Math.random() * 3 + 1);
+
+alert(`Maquina joga como ${jogadas[maquina - 1]}`);
+
+if (isNaN(player)) {
+  alert('Valor Invalido');
+} else if (player === maquina) {
+  alert('Empate');
+} else if ((player === 1 || maquina === 1) && (maquina === 2 || player === 2)) {
+  alert('Papel cobre pedra');
+} else if ((player === 2 || maquina === 2) && (maquina === 3 || player === 3)) {
+  alert('Tesoura corta papel');
+} else if ((player === 1 || maquina === 1) && (maquina === 3 || player === 3)) {
+  alert('Pedra quebra tesoura');
+}
